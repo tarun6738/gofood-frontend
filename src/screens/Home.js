6ca91f8337759252172fef5e42c3c14c1f140bef
Tarun Kumar <tarunnemali2004@gmail.com>
2024-11-04 20:30:41 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+
+
+const Home = () => {
+  const url = "http://localhost:5000";
+  const [search, setSearch] = useState('');
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch(url + "/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response = await response.json();
+    
+    setFoodItem(response[0]);
+
+    setFoodCat(response[1]);
+
+    
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e)=>{setSearch(e.target.value)}}
+                />
+                
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="/burger.jpg"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="/pizza.jpeg"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="/fries.jpeg"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        {!foodCat.isEmpty ? (
+          foodCat.map((data) => {
+            return (
+              <div key={data._id} className="row mb-3">
+                <div className="fs-3 m-3">{data.CategoryName}</div>
+                <hr />
+                {!foodItem.isEmpty ? (
+                  foodItem
+                    .filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase())))
+                    .map((filterItems) => {
+                      return (
+                        <div
+                          key={filterItems._id}
+                          className="col-12 col-md-6 col-lg-4"
+                        >
+                          <Card
+                            foodItem= {filterItems}
+                            options={filterItems.options[0]}
+                            
+                          ></Card>
+                        </div>
+                      );
+                    })
+                ) : (
+                  <div>No such data found</div>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <div>""""""""""</div>
+        )}
+      </div>
+      <div>
+        
+        <Footer></Footer>
+      </div>
+    </>
+  );
+};
+
+export default Home;
+
+
---
+++ src/screens/MyOrder.js
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function MyOrder() {
+  const [orderData, setorderData] = useState({});
+  const [foodItem, setFoodItem] = useState([]);
+  const url = "http://localhost:5000";
+  const loadData = async () => {
+    try {
+      const response = await fetch(url + "/api/foodData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+      setFoodItem(data[0]); // Update food items state
+    } catch (error) {
+      console.error("Error fetching food data:", error);
+    }
+  };
+  
+  const fetchMyOrder = async () => {
+    
+    
+    await fetch(url + "/api/myorderData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: localStorage.getItem("userEmail"),
+      }),
+    }).then(async (res) => {
+      let response = await res.json();
+      await setorderData(response);
+    });
+  };
+  
+
+  useEffect(() => {
+    loadData();
+    fetchMyOrder();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {orderData &&
+          orderData.orderData &&
+          orderData.orderData.order_data ? (
+            orderData.orderData.order_data
+              .slice(0)
+              .reverse()
+              .map((item, index) => (
+                <div key={index}>
+                  {item[0].order_date ? (
+                    <div className="m-auto mt-5">
+                      
+                      {item[0].order_date}{" "}
+                      
+                      <hr />
+                    </div>
+                  ) : null}
+                  {item.slice(1).map((arrayData, idx) => (
+                    <div key={idx}>
+                      <div
+                        className="col-12 col-md-6 col-lg-3"
+                        key={arrayData._id}
+                      >
+                        <div
+                          className="card mt-3"
+                          style={{ width: "16rem", maxHeight: "360px" }}
+                        >
+                          
+                          {foodItem.find((food) => food._id === arrayData.id) && (
+                          <img
+                            src={
+                              foodItem.find(
+                                (food) => food._id === arrayData.id
+                              ).img
+                            }
+                            className="card-img-top"
+                            alt="Food item"
+                            style={{ height: "120px", objectFit: "fill" }}
+                          />
+                        )}
+                          <div className="card-body">
+                            <h5 className="card-title">{arrayData.name}</h5>
+                            <div
+                              className="container w-100 p-0"
+                              style={{ height: "38px" }}
+                            >
+                              <span className="m-1">{arrayData.qty}</span>
+                              <span className="m-1">{arrayData.size}</span>
+                              <div className="d-inline ms-2 h-100 w-20 fs-5">
+                                ₹{arrayData.price}/-
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))
+          ) : (
+            <p>No order data found.</p>
+          )}
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
